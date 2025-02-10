c95cbf39bc407a07cb427e844d025da3dd2408c9
Ageu Assunção Simões Galindo <75844733+Bulovask@users.noreply.github.com>
2025-02-10 20:46:13 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
 package MercatorKairos.src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       
+        Scanner in = new Scanner(System.in);
+        label:
+        while (true) {
+            System.out.print("""
+                    1) Novo Jogo
+                    2) Carregar
+                    3) Sair
+                    =>\s""");
+            String opt = in.nextLine();
+
+            switch (opt) {
+                case "3":
+                    System.out.println("Saindo...");
+                    break label;
+                case "1":
+                    System.out.println("Criando Novo Jogo...");
+                    break;
+                case "2":
+                    System.out.println("Escolha:");
+                    break;
+                default:
+                    System.out.println("Comando desconhecido");
+                    break;
+            }
+        }
     }
 }
