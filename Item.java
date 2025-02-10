90aac64c4afafb06cc2319e71fc8c04f063e26ca
thiagonatalicio <thiagonatalicio33@gmail.com>
2025-02-10 19:58:57 -0300

    criei umas coisas

---
+++ Item.java
@@ -0,0 +1,7 @@
+package MercatorKairos;
+
+public class Item {
+    private String nome;
+    private String tipo;
+    private float preco;
+}
--- Player.java
+++ Player.java
@@ -1,4 +1,35 @@
 package MercatorKairos;
 
 public class Player {
+    //Atributos.
+    private String nome;
+    private int vida;
+    private int forca;
+    private int velocidade;
+    private Item[] inventario [10];
+
+    //Métodos especiais.
+
+    public int getForca() {
+        return forca;
+    }
+
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
